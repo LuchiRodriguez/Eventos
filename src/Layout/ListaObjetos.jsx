33c4039411bda67bd776771c676962ade5073f11
Andrea <andreamania10@gmail.com>
2024-06-18 15:55:40 +0200

    andrealayout

---
+++ src/Layout/ListaObjetos.jsx
@@ -0,0 +1,11 @@
+//QUÉ VOY A LLEVAR EN EL EVENTO
+
+import React from 'react'
+
+const ListaObjetos = () => {
+    return (
+        <div>ListaObjetos</div>
+    )
+}
+
+export default ListaObjetos
---
+++ src/Layout/Registrar.jsx
@@ -0,0 +1,12 @@
+//LOGIN Y REGISTRO.
+//HAY QUE DEJAR EL USUARIO DISPONIBLE A LO LARGO DE TODA LA APLICACIÓN
+
+import React from 'react'
+
+const Registrar = () => {
+    return (
+        <div>Registrar</div>
+    )
+}
+
+export default Registrar
