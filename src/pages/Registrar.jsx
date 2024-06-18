771f9a0db534faf64cae9d9e67422d4869809671
DiegoV97 <velazquezdiego1502@gmail.com>
2024-06-18 17:20:21 +0200

    recarga Andrea

--- src/pages/Registrar.jsx
+++ src/pages/Registrar.jsx
@@ -1,12 +1,34 @@
 //LOGIN Y REGISTRO.
 //HAY QUE DEJAR EL USUARIO DISPONIBLE A LO LARGO DE TODA LA APLICACIÓN
 
-import React from 'react'
+import { useState } from "react"
+import AccesoPopUp from "../components/AccesoPopUp"
 
-const Registrar = () => {
+
+
+const Acceso = () => {
+
     return (
-        <div>Registrar</div>
+        <Container>
+            <h1>Bienvenido a BarnaEvents</h1>
+            <div>
+                <form>
+                    <label for="username">username</label><br></br>
+                    <input type="text" id="username" />
+
+                    <label for="password" id="password">password</label>
+                    <input type="text" id="password" />
+                    <button onClick={ }>Logueate</button>
+                </form>
+
+            </div>
+
+
+            <button onClick={<AccesoPopUp />}>Crear Usuario</button>
+
+
+        </Container>
     )
 }
 
-export default Registrar
+export default Acceso
