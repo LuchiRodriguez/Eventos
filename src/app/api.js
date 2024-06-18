37c766d57aa7a0cda5c2c254c3e5d29459b8ea8a
Andrea <andreamania10@gmail.com>
2024-06-18 18:24:03 +0200

    modificacionesapiyrouter

--- src/app/api.js
+++ src/app/api.js
@@ -2,28 +2,17 @@
 import axios from 'axios';
 const instance = axios.create({ baseURL: 'http://localhost:8080/NOMBRE' });
 
-<<<<<<< HEAD
-axios.post("http://localhost:8080/login", {},
-  {
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: "basic " + btoa(userName + ":" + password),
-    },
-  }
-).then((response) => console.log(response), (error) => console.log(error));
-=======
 export const loguear = async (userName, password) => {
-    await axios.post("http://localhost:8080/login", {},
-        {
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: "basic " + btoa(userName + ":" + password),
-            },
-        }
-    ).then((response) => console.log(response), (error) => console.log(error));
+  await axios.post("http://localhost:8080/login", {},
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "basic " + btoa(userName + ":" + password),
+      },
+    }
+  ).then((response) => console.log(response), (error) => console.log(error));
 }
 
->>>>>>> 41631ce0c4666131cc6e7491403e4902a6872ded
 
 // CREATE
 export const createEvento = async (obj) => await instance.post('/', obj);
@@ -34,10 +23,5 @@ export const getEventos = async () => await instance.get('/');
 // DELETE
 export const deleteEvento = async (id) => await instance.delete('/' + id)
 
-<<<<<<< HEAD
-//CREATE PERFIL 
-export const createPerfil = async (obj) => await instance.post('/', obj);
-=======
 // CREATE PERFIL
 export const createPerfil = async (obj) => await instance.post('/', obj);
->>>>>>> 41631ce0c4666131cc6e7491403e4902a6872ded
