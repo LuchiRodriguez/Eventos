59c0a9c1acf31fb5d6f8be4192baafc635ba4229
Lulirodri98 <106028055+Lulirodri98@users.noreply.github.com>
2024-06-18 18:40:26 +0200

    Lista eventos

--- src/pages/ListaEventos.jsx
+++ src/pages/ListaEventos.jsx
@@ -1,10 +1,35 @@
 //PAGINA DE LISTADO DE EVENTOS. 
 
-import React from 'react'
-
 const ListaEventos = () => {
     return (
-        <div>ListaEventos</div>
+        <div>
+            <nav>
+                <div>
+                    <img src="../app/images/userIcon.png" alt="" />
+                </div>
+                <div>
+                    <img src="../app/images/menuIcon.png" alt="" />
+                </div>
+            </nav>
+            <div>
+                <h1>Listado de eventos</h1>
+                <section>
+                    <div>
+                        <h2>Viernes de juegos</h2>
+                        <button>+</button>
+                    </div>
+                    <div>
+                        <h2>Noche de hamburguesas</h2>
+                        <button>+</button>
+                    </div>
+                    <div>
+                        <h2>Domingo de asadito</h2>
+                        <button>+</button>
+                    </div>
+                </section>
+                <button>Crear evento</button>
+            </div>
+        </div>
     )
 }
 
--- src/pages/Registrar.jsx
+++ src/pages/Registrar.jsx
@@ -1,8 +1,6 @@
 //LOGIN Y REGISTRO.
 //HAY QUE DEJAR EL USUARIO DISPONIBLE A LO LARGO DE TODA LA APLICACIÓN
 
-import React from 'react'
-
 const Registrar = () => {
     return (
         <div>Registrar</div>
