12ede6bb799c01e77aa036f22197bd5697d2ac29
Lulirodri98 <106028055+Lulirodri98@users.noreply.github.com>
2024-06-18 15:53:56 +0200

    creacion de api.js

---
+++ src/app/api.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-undef */
+import axios from 'axios';
+const instance = axios.create({ baseURL: 'http://localhost:8080/NOMBRE' });
+
+axios.post("http://localhost:8080/login",{},
+  {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "basic " + btoa(userName + ":" + password),
+    },
+  }
+).then((response) => console.log(response),(error) => console.log(error));
+
+// CREATE
+export const createEvento = async (obj) => await instance.post('/', obj);
+
+// READ
+export const getEventos = async () => await instance.get('/');
+
+// DELETE
+export const deleteEvento = async (id) => await instance.delete('/' + id)
