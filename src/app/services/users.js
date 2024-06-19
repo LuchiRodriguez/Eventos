e53b78af9e32d245e3a38acf5747cd871a277d25
Andrea <andreamania10@gmail.com>
2024-06-19 17:00:12 +0200

    subida

--- src/app/services/users.js
+++ src/app/services/users.js
@@ -9,3 +9,6 @@ export const loguear = async (userName, password) => {
     }
   ).then((response) => console.log(response), (error) => console.log(error));
 }
+
+//INSCRIBIR EVENTO
+export const inscribirPersona = async (id) => await loguear.post('/', id) //aquí sería userId
