b1e78f8f34e52bc66593ed5369563e20a811bf2f
Andrea <andreamania10@gmail.com>
2024-06-19 16:42:05 +0200

    commit "andreaactualizaciones"

--- src/components/InscribirPersona.jsx
+++ src/components/InscribirPersona.jsx
@@ -2,15 +2,16 @@
 import { useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { inscribirPersona } from "../app/api";
+import { useUserContext } from '../app/providers/UserProvider';
 
 const InscribirPersona = () => {
     const navigate = useNavigate();
-    const { id } = useContext(UserContext); //aquí sería user
+    const { user } = useContext(useUserContext); //aquí sería user
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
 
     const inscripcion = async () => {
-        if (!id) { //aquí sería user
+        if (!user) { //aquí sería user
             setError("No estás autentificado");
             return;
         }
@@ -19,7 +20,7 @@ const InscribirPersona = () => {
         setError(null);
 
         try {
-            await inscribirPersona({ id }); //aquí sería userId : user.id
+            await inscribirPersona({ userId: user.id });
             navigate('/');
         } catch (err) {
             setError('Hubo un problema al inscribirte al evento. Inténtalo de nuevo');
