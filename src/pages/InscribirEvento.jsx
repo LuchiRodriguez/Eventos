fe3454fbb3133c35df2f479db39b721400d7f7b5
Andrea <andreamania10@gmail.com>
2024-06-19 18:18:39 +0200

    subida

--- src/pages/InscribirEvento.jsx
+++ src/pages/InscribirEvento.jsx
@@ -3,13 +3,28 @@
 //TIENE QUE REGISTRAR EN EL EVENTO. 
 
 //dieta especial, acompañante (si/no, y cuantas personas (boton con números))
+import { useContext } from "react";
+import { inscribirPersona } from "../app/services/users";
+import { useUserContext } from "../app/providers/UserProvider";
+import { useNavigate } from "react-router-dom";
+
+//TIENE QUE REGISTRAR EN EL EVENTO. 
+
+//dieta especial, acompañante (si/no, y cuantas personas (boton con números))
 
 const InscribirEvento = () => {
+    const { userId } = useContext(useUserContext);
+    const navigate = useNavigate();
 
     return (
+        <div>Inscribirse usuario al evento
+            <button onClick={async () => {
+                await inscribirPersona({ userId });
+                navigate('/');
 
-        <div>Inscribirse usuario al evento</div>
+            }}></button>
 
+        </div>
         //INSCRIBIR
         //perfil, evento
 
