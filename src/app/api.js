14805c0b7753cfebf5a2ee53659cd0f0f47131f1
DiegoV97 <velazquezdiego1502@gmail.com>
2024-06-19 16:58:32 +0200

    actualizar luchi

--- src/app/api.js
+++ src/app/api.js
@@ -2,6 +2,11 @@
 import axios from 'axios';
 const instance = axios.create({ baseURL: 'http://localhost:8080/NOMBRE' });
 
+export const setUserAndPassword = (user, password) => {
+  i.default.headers.common.username = user;
+  i.default.headers.common.password = password;
+};
+
 export const loguear = async (userName, password) => {
   await axios.post("http://localhost:8080/login", {},
     {
